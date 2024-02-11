718f3829aca505c37c941d3e208a09e85505e747
virtualblakewillis <bewillis101@gmail.com>
2024-02-11 15:57:10 -0600

    removed extra newline

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -4,6 +4,6 @@ String showFunc(int a, int b) {
   int addResult = add(a, b);
   int subResult = sub(a, b);
   String result = 'Add $a + $b = $addResult\n';
-  result += 'Sub $a - $b = $subResult\n';
+  result += 'Sub $a - $b = $subResult';
   return result;
 }
